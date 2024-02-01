5b68ad4845b52825511683b2ade97943025baa11
theskyworld <2456450687@qq.com>
2024-02-01 14:21:28 +0800

    feat: add Greeting.vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
+  integrations: [vue()]
 });
---
+++ src/components/Greeting.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// @ts-nocheck
+import { ref, computed } from "vue";
+
+const { messages } = defineProps(["messages"]);
+
+const greeting = ref(messages[0]);
+function changeGreeting() {
+  const randomMessage = computed(
+    () => messages[Math.floor(Math.random() * messages.length)]
+  );
+  greeting.value = randomMessage.value;
+}
+</script>
+<template>
+  <div>
+    <h3>{{ greeting }}! Thank you for visiting!</h3>
+    <button @click="changeGreeting">New Greeting</button>
+  </div>
+</template>
+<style scoped lang="scss"></style>
